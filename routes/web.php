22a8e7d50b81e15b100c34b10ff0d1c0e2f79c9a
alirfanyasin <risayunita21@gmail.com>
2025-04-28 11:51:17 +0700

    Integration template Dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,48 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+
+Route::get('/dashboard', function () {
+    $data = [
+        'title' => 'Dashboard',
+    ];
+    return view('pages.index', $data);
+})->name('dashboard');
+
+Route::prefix('app')->group(function () {
+    Route::get('/calendar', function () {
+        $data = [
+            'title' => 'Calendar',
+        ];
+        return view('pages.app-calendar', $data);
+    })->name('app-calendar');
+
+    Route::get('/gallery', function () {
+        $data = [
+            'title' => 'Gallery',
+        ];
+        return view('pages.app-gallery', $data);
+    })->name('app-gallery');
+
+    Route::get('/plans', function () {
+        $data = [
+            'title' => 'Pricing Plans',
+        ];
+        return view('pages.app-plans', $data);
+    })->name('app-plans');
+
+    Route::get('/contacts', function () {
+        $data = [
+            'title' => 'Contacts',
+        ];
+        return view('pages.app-contacts', $data);
+    })->name('app-contact');
+});
+
+
+Route::get('/starter-page', function () {
+    return view('pages.starter-page');
+})->name('starter-page');
